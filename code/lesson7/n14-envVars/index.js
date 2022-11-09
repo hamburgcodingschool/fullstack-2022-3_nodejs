7601d9ea386efacd8294ba2963c6763a760a7443
Helder Pereira <phelder@gmail.com>
2022-11-09 20:05:33 +0000

    final for today

---
+++ code/lesson7/n14-envVars/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+console.log(process.env.SOME_NAME);
+console.log(process.env.SOME_TEXT);
+console.log(process.env.PASSWORD);
+console.log(process.env.SOMETHING_ELSE);
--- code/restExample/serverApp/src/databases/db.js
+++ code/restExample/serverApp/src/databases/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "12345678",
-    database: "my_first_db"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = {
