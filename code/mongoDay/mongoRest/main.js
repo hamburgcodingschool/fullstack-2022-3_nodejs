902b443cea6c4524f1635624d5c054d8b3a29663
Helder Pereira <phelder@gmail.com>
2022-12-10 16:21:19 +0100

    fixed port

--- code/mongoDay/mongoRest/main.js
+++ code/mongoDay/mongoRest/main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const moviesDB = require("./moviesDB");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", async function(req, res) {
     // moviesDB.findLordMovies(function(result) {
